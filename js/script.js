bca0a96de36e3a175024e7486995dfef78fad610
Luiza Rabuski <luiza.rabuski@gmail.com>
2023-07-26 19:34:17 -0300

    Adicionada pontuação

--- js/script.js
+++ js/script.js
@@ -1,25 +1,35 @@
 
 $(document).keydown(function(event) {
-    $('.mario').addClass('jump');
+    const mario = $('.mario');
+    mario.addClass('jump');
     setTimeout(function() {
-        $('.mario').removeClass('jump');
+        mario.removeClass('jump');
     }, 500);
 })
 
+var pontuacao = 1;
+var loopPontuacao = setInterval(function(){
+    $('.pontuacao').text(pontuacao++);
+}, 1500);
+
 var loop = setInterval(function(){
     const posicaoPipe = +$('.pipe').offset().left;
-    const posicaoMario = +$('.mario').css('bottom').replace('px', '');
-    console.log(posicaoPipe);
+    const mario = $('.mario');
+    const posicaoMario = +mario.css('bottom').replace('px', '');
+
     if(posicaoPipe <= 120 && posicaoMario < 80 && posicaoPipe > 0){
         $('.pipe').css('animation','none');
         $('.pipe').css('left', posicaoPipe);
 
-        $('.mario').css('animation', 'none');
-        $('.mario').css('bottom', posicaoMario);
+        mario.css('animation', 'none');
+        mario.css('bottom', posicaoMario);
 
-        $('.mario').attr('src', 'img/game-over.png');
-        $('.mario').css('width', '75px');
-        $('.mario').css('margin-left', '50px');
+        mario.attr('src', 'img/game-over.png');
+        mario.css('width', '75px');
+        mario.css('margin-left', '50px');
 
-    }
+        clearInterval(loop);
+        clearInterval(loopPontuacao);
+
+    } 
 }, 10);
